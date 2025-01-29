4c6faa2e52c6221c83c552c4c167b449df1f6132
Anjalinegi <anjalinegi26500@gmail.com>
2025-01-29 10:02:51 +0530

    Update Area.java

--- Area.java
+++ Area.java
@@ -1,12 +1,20 @@
-import java.util.Scanner;
+import java.util.Scanner;  // Importing the Scanner class to take user input
 
 public class Area {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        // Creating a Scanner object to read input from the user
+        Scanner sc = new Scanner(System.in);
+
+        // Prompting the user to enter the values for length and breadth
         System.out.println("enter values of a and b:");
-        float length=sc.nextFloat();
-        float breadth=sc.nextFloat();
+
+        // Reading the length and breadth values from the user
+        float length = sc.nextFloat();   
+        float breadth = sc.nextFloat();  
+
+        // Closing the scanner object 
         sc.close();
-        System.out.println("area of rectangle:"+ length*breadth);
+
+        System.out.println("area of rectangle: " + length * breadth);  // Output the area
     }
 }
